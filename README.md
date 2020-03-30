dd61394d08906f7cfbdaa139a5262ab0ce51a389
kesc <492167585@qq.com>
2020-03-30 17:48:44 +0800

    支持一对多的扁平数据转换成es嵌套对象。

---
+++ README.md
@@ -0,0 +1,5 @@
+本插件仅在Elasticsearch 5.x上测试，详细文档请查看doc目录。
+
+
+
+
---
+++ src/main/java/com/alibaba/datax/plugin/writer/elasticsearchwriter/ESColumn.java
@@ -0,0 +1,121 @@
+package com.alibaba.datax.plugin.writer.elasticsearchwriter;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Created by xiongfeng.bxf on 17/3/2.
+ */
+public class ESColumn {
+
+    private String name;//: "appkey",
+
+    private String type;//": "TEXT",
+
+    private String timezone;
+
+    private String format;
+
+    private Boolean array;
+
+    private List<ESColumn> child;
+
+    private Integer colNo;
+
+    private Boolean ignore;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setTimeZone(String timezone) {
+        this.timezone = timezone;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getTimezone() {
+        return timezone;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setTimezone(String timezone) {
+        this.timezone = timezone;
+    }
+
+    public Boolean isArray() {
+        return array;
+    }
+
+    public void setArray(Boolean array) {
+        this.array = array;
+    }
+
+    public Boolean getArray() {
+        return array;
+    }
+
+    public List<ESColumn> getChild() {
+        return child;
+    }
+
+    public void setChild(List<ESColumn> child) {
+        this.child = child;
+    }
+
+    public boolean hasChild() {
+        return child != null && !child.isEmpty();
+    }
+
+    public Integer getColNo() {
+        return colNo;
+    }
+
+    public void setColNo(Integer colNo) {
+        this.colNo = colNo;
+    }
+
+    public Boolean getIgnore() {
+        return ignore;
+    }
+
+    public void setIgnore(Boolean ignore) {
+        this.ignore = ignore;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ESColumn column = (ESColumn) o;
+        return colNo == column.colNo &&
+                Objects.equals(name, column.name) &&
+                Objects.equals(type, column.type) &&
+                Objects.equals(timezone, column.timezone) &&
+                Objects.equals(format, column.format) &&
+                Objects.equals(array, column.array) &&
+                Objects.equals(child, column.child);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, type, timezone, format, array, child, colNo);
+    }
+}
